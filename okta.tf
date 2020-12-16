602fe2745d84e38e4d7e4eb77d5187e5c8f569ba
45416631+mallikarjun1006@users.noreply.github.com <45416631+mallikarjun1006@users.noreply.github.com>
2020-12-16 14:16:09 +0530

    delete

--- okta.tf
+++ okta.tf
@@ -1,16 +1,4 @@
-resource "okta_user" "example" {  
-  email ="terreform@example.com"
-  login = "terreform@example.com"
-  first_name = "terraform"
-  last_name = "example"
-}
 
-resource "okta_user" "example1" {  
-  email ="terreform@example1.com"
-  login = "terreform@example1.com"
-  first_name = "terraform1"
-  last_name = "example1"
-}
 
 terraform {
   required_providers {
