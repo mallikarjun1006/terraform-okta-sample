45b99a062782c2cb5b49f5a8895db9fee1cc39b6
45416631+mallikarjun1006@users.noreply.github.com <45416631+mallikarjun1006@users.noreply.github.com>
2020-12-16 12:58:14 +0530

    removed_unsuppoted arg

--- okta.tf
+++ okta.tf
@@ -1,10 +1,4 @@
-resource "okta_user" "example" {
-  index       = "customPropertyName"
-  title       = "customPropertyName"
-  type        = "string"
-  description = "My custom property name"
-  master      = "OKTA"
-  scope       = "SELF"
+resource "okta_user" "example" {  
   email ="terreform@example.com"
   login = "terreform@example.com"
   first_name = "terraform"
